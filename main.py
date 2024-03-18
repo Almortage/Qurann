bee4bdac3b113a9983429f7ec7c1e8f8cb2f513c
Moalmortagel <105941198+Almortage@users.noreply.github.com>
2024-03-18 16:59:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ REPLY_MESSAGE_BUTTONS = [
     ]
 ]
 
-@app.message_handler(commands=["start"])
+@app.on_message(commands=["start"])
 async def madison(client: Client, message: Message): 
     text = REPLY_MESSAGE
     reply_markup = ReplyKeyboardMarkup(REPLY_MESSAGE_BUTTONS, one_time_keyboard=True, resize_keyboard=True)
