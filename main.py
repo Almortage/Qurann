88c5ba132d3e6473b9ccd1a4a269023ed3dfa0b8
Moalmortagel <105941198+Almortage@users.noreply.github.com>
2024-03-18 16:52:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from config import Config
 from pyrogram import filters
 import random
 from pyrogram import Client
-from pyrogram import app
+import app
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
 from random import  choice, randint
 
